b6b697d2a4caed82eb6accdcdff8f5a8b32fd7ba
seanmx96 <seanmx96@users.noreply.github.com>
2020-08-19 18:01:19 -0500

    Implemented branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,11 @@
 
 import sys
 
+OP_LDI = 0b10000010
+OP_PRN = 0b01000111
+OP_MUL = 0b10100010
+OP_HLT = 0b00000001
+
 class CPU:
     """Main CPU class."""
 
@@ -9,6 +14,12 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0b00000000] * 256
         self.reg = [0b00000000] * 8
+
+        self.branchtable = {}
+        self.branchtable[OP_LDI] = self.ldi
+        self.branchtable[OP_PRN] = self.prn
+        self.branchtable[OP_MUL] = self.mul
+        self.branchtable[OP_HLT] = self.hlt
 
         self.running = True
 
@@ -48,7 +59,6 @@ class CPU:
 
                     try:
                         self.ram[address] = int(temp[0], 2)
-                        # print(f"RAM: {self.ram[address]}")
 
                     except ValueError:
                         print(f"Invalid number: {temp[0]}")
@@ -64,7 +74,7 @@ class CPU:
             print("Program was empty!")
             sys.exit(3)
 
-        # print(f"RAM: {self.ram}")
+        print(f"RAM: {self.ram}")
 
 
     def alu(self, op, reg_a, reg_b):
@@ -109,42 +119,38 @@ class CPU:
         else:
             raise Exception("Unsupported 'i' value")
 
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.incrementPC(3)
+
+    def prn(self, operand_a):
+        print(self.reg[operand_a])
+        self.incrementPC(2)
+
+    def mul(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+        self.incrementPC(3)
+
+    def hlt(self):
+        self.running = False
+        self.incrementPC(1)
 
     def run(self):
         """Run the CPU."""
         while self.running:
             ir = self.ram_read(self.pc)
-            # print(f"IR: {ir}")
 
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+            num_ops = "{0:08b}".format(ir)[0:2]
 
-            # LDI register immediate
-            if ir == 130:
-                self.reg[operand_a] = operand_b
+            if num_ops == '00':
+                self.branchtable[ir]()
 
-                # print("LDI register running")
-                # print(ir)
+            elif num_ops == '01':
+                self.branchtable[ir](operand_a)
 
-                self.incrementPC(3)
-                
-            # Print numeric value stored in the given register
-            elif ir == 71:
-                # print("running")
-                print(self.reg[operand_a])
-
-                self.incrementPC(2)
-
-            elif ir == 162:
-                # print("Running Multiply")
-                self.alu("MUL", operand_a, operand_b)
-
-                self.incrementPC(3)
-
-            # Halt the CPU
-            elif ir == 1:
-                print(f"")
-                self.running = False
-                self.incrementPC(1)
+            elif num_ops == '10':
+                self.branchtable[ir](operand_a, operand_b)
 
 
